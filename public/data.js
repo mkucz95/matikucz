88a1da4059bba4928d3e6432370caec4f71ec487
Mateusz Kucz <mati.kucz95@gmail.com>
2018-04-30 21:23:13 -0700

    UX design changes

--- public/data.js
+++ public/data.js
@@ -311,7 +311,7 @@ var volunteering=[{
 function createEl(x, selector){
     console.log(x+": "+selector);
     let el = document.createElement("section");
-    el.innerHTML=`<h3 onclick=changeView(this)>${x.type}</h3><p><em>${x.code}</em><br>${x.description}</p>`;
+    el.innerHTML=`<h3 onclick=changeView(this)>${x.type}</h3><p style="display:none"><em>${x.code}</em><br>${x.description}</p>`;
     document.getElementById(selector).appendChild(el);
 }
 function changeView(element){
@@ -366,17 +366,31 @@ function changeEvent(event){
         populateCourses();
     }
     else if(event==="experience"){
-        document.querySelector('main>div').innerHTML="<article><h1>Skills</h1><section id='skills'><ul></ul></section><section id='tools'><h2>Tools</h2><ul></ul></section><section id='techskills'><h2>Technologies</h2><ul></ul></section></article><article id='work'><h1>Experience</h1></article><article id='activities'><h1>Activities</h1></article><article id='volunteering'><h2>Volunteering</h2></article><article id='awards'><h2>Honours & Awards</h2></article>";
+        document.querySelector('main>div').innerHTML="<article><h1>Skills</h1 onclick=changeView(this)><section id='skills'><ul></ul></section><section id='tools'><h2>Tools</h2><ul></ul></section><section id='techskills'><h2>Technologies</h2><ul></ul></section></article><article id='work'><h1>Experience</h1></article><article id='activities'><h1>Activities</h1></article><article id='volunteering'><h2>Volunteering</h2></article><article id='awards'><h2>Honours & Awards</h2></article>";
         populateExperience();
     }
-    else if(event==="home"){document.querySelector('main>div').innerHTML=home;}
-    else if(event==="references"){document.querySelector('main>div').innerHTML="<h1>References</h1><dl id='references'></dl>"; 
+    else if(event==="about"){document.querySelector('main>div').innerHTML=about;}
+    else if(event==="references"){document.querySelector('main>div').innerHTML="<article><section><h1>References</h1><dl id='references'></dl></section></article>"; 
         populateRefs();}
+    else if(event==="home"){
+        document.querySelector('main>div').innerHTML=home;
+    }
 }
 const education = "<div id='education'><h1>Education</h1><dl><dt><strong>University of California, San Diego (UCSD) </strong> - Expected June 2018 </dt><dd> B.S. in Management Science</dd><dd> Minor in Computer Science</dd><dd>Overall GPA: 3.72/4.00 - Major GPA: 3.84, Minor GPA: 3.71</dd><dt>Overseas Family School (OFS) - Singapore</dt><dd>IGSCE</dd><dd> International Baccalaureate (IB) - Bilingual Diploma</dd></dl></div>";
 const econCourses ="<article class='coursework' id='econCourses'><h2>Economics Coursework</h2></article>";
 const cseCourses = "<article class='coursework' id='cseCourses'><h2>Computer Science Coursework</h2></article>";
 const mathCourses = "<article class='coursework' id='mathCourses'><h2>Mathematics Coursework</h2></article>";
 const chinCourses = "<article class='coursework' id='chinCourses'><h2>Chinese Language Coursework</h2></article>";
-const home=`<article><img src='${defaultImg}'><section><h1>Current:</h1><p>I am currently studying for a B.S. in Management Science in my 4th year at UC San Diego with a minor in Computer Science. I am also a team capitain for the UCSD Men's Golf team.</p><p>I work as the HR Coordinator for Best Day Foundation in San Diego which works with special needs children providing them with great outdoors experiences.</p><p>In the future I hope to work with the technology and renewable energy sectors since I am passionate about their growth, development, and possible applications and innovations throughout society.</p></section><section><h1>About Me</h1><p>I am currently a student at UC San Diego in my fourth year. I am a passionate golfer and I am proud to be on the UCSD Men's Golf Team. I chose UC San Diego because of it's renowned academic caliber in the fields that interest me most - economics and computer science. The perfect weather and location, challenging intellectual environment, and demanding collegiate golf are all factors that have contributed to my progress and success as a student-athlete and person. I would like to work with businesses, investments, and technologies that deal with social and environmental causes around the world. It is a goal of mine to eventually utilize the languages I know throughout my career. <p>I enjoy cooking and all kinds of outdoor activities in my free time. As a California resident, I started to surf two years ago. I also enjoy tennis, basketball, volleyball, table tennis, hiking, or just being at the beach. I am also a passionate traveller and I hope to make it to three new places every year. South America and Africa are the next two continents I have set my sights on. San Diego, CA - is the 6th city in the 6th different country that I have lived in.
-<li><a href="http://www.ucsdtritons.com/SportSelect.dbml?DB_OEM_ID=5800&SPID=2343&SPSID=29950">UCSD Golf</a></li><li><a href="http://economics.ucsd.edu/undergraduate-program/major-minor-requirements/mgmt-sci.html">Management B.S.</a></li><li><a href="https://cse.ucsd.edu/undergraduate/degree-programs/minor-computer-science">Computer Science Minor</a></li><li><a href="http://www.github.com/mkucz95">My GitHub</a></li><li><a href="https://bestdayfoundation.org/">Best Day Foundation</a></li></p></section></article>`;
+const about=`<article><section><h1>Current</h1><p>I am currently studying for a B.S. in Management Science in my 4th year at UC San Diego with a minor in Computer Science. I am also a team capitain for the UCSD Men's Golf team.</p><p>I work as the HR Coordinator for Best Day Foundation in San Diego which works with special needs children providing them with great outdoors experiences.</p><p>In the future I hope to work with the technology and renewable energy sectors since I am passionate about their growth, development, and possible applications and innovations throughout society.</p></section><section><h1>About Me</h1><p>I am currently a student at UC San Diego in my fourth year. I am a passionate golfer and I am proud to be on the UCSD Men's Golf Team. I chose UC San Diego because of it's renowned academic caliber in the fields that interest me most - economics and computer science. The perfect weather and location, challenging intellectual environment, and demanding collegiate golf are all factors that have contributed to my progress and success as a student-athlete and person. I would like to work with businesses, investments, and technologies that deal with social and environmental causes around the world. It is a goal of mine to eventually utilize the languages I know throughout my career. <p>I enjoy cooking and all kinds of outdoor activities in my free time. As a California resident, I started to surf two years ago. I also enjoy tennis, basketball, volleyball, table tennis, hiking, or just being at the beach. I am also a passionate traveller and I hope to make it to three new places every year. South America and Africa are the next two continents I have set my sights on. San Diego, CA - is the 6th city in the 6th different country that I have lived in.</p></section>
+<section id="contact"> <h1>Contact</h1><a href="tel:1-858-260-4579">(858)-260 4579</a>
+<a href="mailto:mati.kucz95@gmail.com?subject=Reaching Out From Your Website" target="_blank">mati.kucz95@gmail.com</a>
+</section></article>`;
+
+const home=`<img src="headshot.jpg" class="home">
+  <ul class="center" id="home">
+    <li><a href="http://www.ucsdtritons.com/SportSelect.dbml?DB_OEM_ID=5800&SPID=2343&SPSID=29950">UCSD Golf</a></li>
+    <li><a href="http://economics.ucsd.edu/undergraduate-program/major-minor-requirements/mgmt-sci.html">Management B.S.</a></li>
+    <li><a href="https://cse.ucsd.edu/undergraduate/degree-programs/minor-computer-science">Computer Science Minor</a></li>
+    <li><a href="http://www.github.com/mkucz95">My GitHub</a></li>
+    <li><a href="https://bestdayfoundation.org/">Best Day Foundation</a></li>
+  </ul>`;
